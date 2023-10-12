3f329e312792d61c982e77b47d1daf6b6a81fdbc
Vladyslav Parfinenko <ronwaylla@gmail.com>
2023-10-12 17:35:48 +0300

    added home section

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,3 +27,11 @@ const navLink = document.querySelectorAll('.nav__link')
     }
 navLink.forEach(n=>n.addEventListener('click',linkAction));
 
+
+            // Change background header
+ function scrollHeader(){
+ const header = document.getElementById('header')
+                
+if(this.scrollY >= 100) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader);
