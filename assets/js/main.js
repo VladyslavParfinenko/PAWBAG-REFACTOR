ead891b554fd07c812a51db1b57c112cc55e537c
Vladyslav Parfinenko <ronwaylla@gmail.com>
2023-10-13 12:07:23 +0300

    added scroll up

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,80 +1,104 @@
-            // Show menu
+// Show menu
 
 const navMenu = document.getElementById('nav-menu'),
-        navToggle = document.getElementById('nav-toggle'),
-        navClose = document.getElementById('nav-close')
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close');
 
-if(navToggle){
-    navToggle.addEventListener('click',()=>{
-        navMenu.classList.add('show-menu')
-    })
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
 }
 
-            // Hide menu
-if(navClose){
-    navClose.addEventListener('click',()=>{
-        navMenu.classList.remove('show-menu')
-    })
+// Hide menu
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
 }
 
-            // Remove menu
+// Remove menu
 
-const navLink = document.querySelectorAll('.nav__link')
-    function linkAction(){
-        const navMenu = document.getElementById('nav-menu')
-        navMenu.classList.remove('show-menu')
+const navLink = document.querySelectorAll('.nav__link');
+function linkAction() {
+  const navMenu = document.getElementById('nav-menu');
+  navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
 
-    }
-navLink.forEach(n=>n.addEventListener('click',linkAction));
+// Change background header
+function scrollHeader() {
+  const header = document.getElementById('header');
 
-
-            // Change background header
- function scrollHeader(){
- const header = document.getElementById('header')
-                
-if(this.scrollY >= 100) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+  if (this.scrollY >= 100) header.classList.add('scroll-header');
+  else header.classList.remove('scroll-header');
 }
 window.addEventListener('scroll', scrollHeader);
 
+// SWIPER Discover
+var swiper = new Swiper('.discover__container', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  loop: true,
+  spaceBetween: 32,
+  coverflowEffect: {
+    rotate: 0,
+  },
+});
 
-            // SWIPER Discover
-            var swiper = new Swiper(".discover__container", {
-                effect: "coverflow",
-                grabCursor: true,
-                centeredSlides: true,
-                slidesPerView: "auto",
-                loop:true,
-                spaceBetween:32,
-                coverflowEffect: {
-                  rotate: 0,
-                },
-                
-              });
-
-
-
-            //   VIDEO
+//   VIDEO
 const videoFile = document.getElementById('video-file'),
-        videoButton = document.getElementById('video-button'),
-        videoIcon = document.getElementById('video-icon')
-function playPause(){
-    if(videoFile.paused){
-        videoFile.play()
+  videoButton = document.getElementById('video-button'),
+  videoIcon = document.getElementById('video-icon');
+function playPause() {
+  if (videoFile.paused) {
+    videoFile.play();
 
-        videoIcon.classList.add('ri-pause-line')
-        videoIcon.classList.remove('ri-play-line')
-    }
-    else{
-        videoFile.pause()
-        videoIcon.classList.remove('ri-pause-line')
-        videoIcon.classList.add('ri-play-line')
-    }
+    videoIcon.classList.add('ri-pause-line');
+    videoIcon.classList.remove('ri-play-line');
+  } else {
+    videoFile.pause();
+    videoIcon.classList.remove('ri-pause-line');
+    videoIcon.classList.add('ri-play-line');
+  }
 }
 
-videoButton.addEventListener('click',playPause)
+videoButton.addEventListener('click', playPause);
 
-function finalVideo(){
-    videoIcon.classList.remove('ri-pause-line')
-    videoIcon.classList.add('ri-play-line')
+function finalVideo() {
+  videoIcon.classList.remove('ri-pause-line');
+  videoIcon.classList.add('ri-play-line');
 }
-videoFile.addEventListener('ended',finalVideo)
+videoFile.addEventListener('ended', finalVideo);
+
+// Scroll UP
+function scrollUp() {
+  const scrollUp = document.getElementById('scroll-up');
+
+  if (this.scrollY >= 200) scrollUp.classList.add('show-scroll');
+  else scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp);
+
+// Scroll section active link
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
